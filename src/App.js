e2ab310424eaef13ddf17e48282a100b8a5f534c
gabmor38 <Gabriela.morenor@gmail.co>
2022-03-09 15:03:10 -0500

    set up of landing page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Profile from './components/Home/Profile'
+import './App.css';
+
+function App() {
+  return (
+    <div className ="App">
+    <Profile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Profile.js
@@ -0,0 +1,50 @@
+import Typical from 'react-typical'
+
+
+
+function Profile() {
+  return (
+    <div className ="profile_container">
+      <div className = "profile-parent">
+        <div className = "profile-details">
+          <div className = "colz">
+            <a href="#"><i className= "fa fa-facebook-square"/></a>
+            <a href="#"><i className = "fa fa-google-plus-square"/></a>
+          </div>
+            <div className ="profile-details-name">
+              <span className = "primary-text">
+              {" "}
+              Hello, I am <span className="highlighted-text">Gabriela Moreno</span>
+              </span>
+            </div>
+            <div className="profile-details-role">
+              {" "}
+              <span>
+                <h1>
+                {" "}
+                <Typical
+                loop={Infinity}
+                steps={[
+                  "Enthusiastic 😀",1000,
+                  "Full Stack Developer 💻",1000,
+                  "PERN Stack Developer 😎",1000,
+                ]}
+                />
+                </h1>
+                <span className='profile-role-tagline'></span>
+                Building applications with front and back operations
+              </span>
+            </div>
+            <div className="profile-options">
+              <button className="btn btn-primary">{" "} Hire Me! {" "}</button>
+              <a href="resume" download ="resume.pdf">
+                <button className ="btn btn-highlighted">Download Resume</button>
+              </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
