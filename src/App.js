8d5b81dd21cba51ee7896e52cf8eb0bb6647ab4b
gabmor38 <Gabriela.morenor@gmail.co>
2022-03-10 19:44:15 -0500

    created footer to main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import Profile from './components/Home/Profile'
+
+import Home from './components/Home/Home'
+
 import './App.css';
 
 function App() {
   return (
     <div className ="App">
-    <Profile/>
+    <Home/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-parent">
+        <img src={ require("../../assets/fonts/shape-bg.png")} alt="HELLO"/>
+      </div>
+    </div>
+  )
+}
