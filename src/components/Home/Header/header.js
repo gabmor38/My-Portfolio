8e4ec7913a6d2d143baa62892e0d60d8be695094
gabmor38 <Gabriela.morenor@gmail.com>
2022-03-17 23:13:32 -0400

    removed testing string

---
+++ src/components/Home/Header/header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Total_Screens, Get_Screen_Index  } from '../../../Utilities/commonUtils';
+import ScrollService from '../../../Utilities/ScrollService';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Header.css'
+import { isFulfilled } from 'q';
+
+export default function Header() {
+
+  const [selectedScreen, setSelectedScreen]  = useState(0);
+  const [showHeaderOptions, setHeaderOptions] = useState(false);
+
+  //function to updateScreen
+  const UpdateCurrentScreen = (currentScreen) => {
+    if(!currentScreen || !currentScreen.screenInView)
+      return;
+    let screenIndex = Get_Screen_Index(currentScreen.screenInView)
+     if(screenIndex < 0)
+     return;
+  }
+
+  let currentScreenSubscription = ScrollService.currentScreenBoradcaster.subscribe(UpdateCurrentScreen)
+
+  //function to update navbar names dynamically
+
+  const getHeaderOptions = () => {
+    return (
+      Total_Screens.map((screen, i) => {
+        <div className={getHeaderOptions(i)}
+          key={screen.screen_name}
+          onClick={()=> switchScreen(i, screen)}
+          >
+            <span>{screen.screen_name}</span>
+        </div>
+      })
+    )
+  }
+
+  return (
+    <>
+    <div>
+    
+    </div>
+    
+    </>
+  );
+}
