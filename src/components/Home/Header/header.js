f066bb116d421ba1fdf2f8a2ecad9a6107ce64f4
gabmor38 <Gabriela.morenor@gmail.co>
2022-03-17 22:33:28 -0400

    created function to update screen

--- src/components/Home/Header/header.js
+++ src/components/Home/Header/header.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Total_Screens, Get_Screen_Index  } from '../../../Utilities/commonUtils';
+import ScrollService from '../../../Utilities/ScrollService';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Header.css'
+import { isFulfilled } from 'q';
 
 export default function Header() {
+
+  const [selectedScreen, setSelectedScreen]  = useState(0);
+  const [showHeaderOptions, setHeaderOptions] = useState(false);
+
+  //function to updateScreen
+  const UpdateCurrentScreen = (currentScreen) => {
+    if(!currentScreen || !currentScreen.screenInView)
+      return;
+    let screenIndex = Get_Screen_Index(currentScreen.screenInView)
+     if(screenIndex < 0)
+     return;
+  }
+
+  let currentScreenSubscription = ScrollService.currentScreenBoradcaster.subscribe(UpdateCurrentScreen)
+
   return (
     <>
     <div>
-      
+
     </div>
     
     </>
