c12c96e340e9564de899b635b677316ba1d9cdf5
gabmor38 <Gabriela.morenor@gmail.com>
2022-04-22 21:23:04 -0400

    Styled AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,59 +1,75 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import ScreenHeading from '../../Utilities/ScreenHeading/ScreenHeading';
 import ScrollService from '../../Utilities/ScrollService';
 import Animations from '../../Utilities/Animations';
 
 export default function AboutMe(props) {
   let fadeInScreenHandler = (screen) => {
-    if(screen.fadeScreen !== props.id) 
-      return;
+    if(screen.fadeInScreen !== props.id)
+    return;
     Animations.animations.fadeInScreen(props.id);
-  };
+} 
+const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-  const fadeInSubscription = 
-    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
-
-  const screenConstants = {
-    description: "Description about yourself",
+const screenConstants = { 
+    description: "Full stack web and mobile developer with background knowledge of MERN stacks, along with a knack of building applications.",
     highlights: {
-      bullets: [
-        "1",
-        "2",
-        "3",
-        "4",
-      ],
-      heading: "Title of highlights"
+        bullets: [
+            "Full Stack web and mobile development",
+            "Interactive Front End as per the design",
+            "React and React Native",
+            "Redux for State Mnanagement",
+            "Building REST API",         
+            "Managing database",
+       ],
+       heading: "Here are a Few Highlights:"
     }
-  }
+}
 
-  const renderHighlights= () => {
+  const renderHighlights = () => {
     return (
-      screenConstants.highlights.bullets.map((value, i) =>{ 
-        <div className="highlight" key={i} >
-          <div className="highlight-blob"> 
-            <span>{value}</span>
-          </div>
-        </div>
-      } )
-    ) 
-  }
+        screenConstants.highlights.bullets.map((value,i) => (
+            <div className="highlight" key={i}>
+                <div className="highlight-blob"></div>
+                <span>{ value }</span>
+            </div>
+        ))
+    )
+}
 
+  useEffect(() => {
+    return () => {
+      /* UNSUBSCRIBE THE SUBSCRIPTIONS */
+      fadeInSubscription.unsubscribe();
+    };
+  }, [fadeInSubscription]);
 
 
   return (
     <>
-    <div className="about-me-container screen-container" id={props.id || ""}>
-      <div className="about-me-parent">
-        <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'}/>
-        <div className="about-me-card">
-          <div className="about-me-profile">
+      <div className="about-me-container screen-container fade-in" id={props.id || ''}>
+        <div className="about-me-parent">
+          <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'} />
+          <div className="about-me-card">
+            <div className="about-me-profile"></div>
             <div className="about-me-details">
               <span className="about-me-description">{screenConstants.description}</span>
+              <div className="about-me-highlights">
+                <div className="highlight-heading">
+                  <span>{screenConstants.highlights.heading}</span>
+                </div>
+                {renderHighlights()}
+              </div>
+              <div className="about-me-options">
+                <button className="btn primary-btn" onClick={() => ScrollService.scrollHandler.scrollToHireMe()}> Hire Me </button>
+                <a href="cv.pdf" download="Resume.pdf">
+                  <button className="btn highlighted-btn"> Get Resume </button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
