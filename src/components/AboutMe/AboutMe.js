efcbaf14be7a94c6fdd2d82cd172c9f88ca2675a
gabmor38 <Gabriela.morenor@gmail.com>
2022-04-22 22:18:03 -0400

    fixed spelling

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,37 +6,37 @@ import '../AboutMe/AboutMe.css';
 
 export default function AboutMe(props) {
   let fadeInScreenHandler = (screen) => {
-    if(screen.fadeInScreen !== props.id)
-    return;
+    if (screen.fadeInScreen !== props.id)
+      return;
     Animations.animations.fadeInScreen(props.id);
-} 
-const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+  };
+  const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-const screenConstants = { 
+  const screenConstants = {
     description: "Full stack web and mobile developer with background knowledge of MERN stacks, along with a knack of building applications.",
     highlights: {
-        bullets: [
-            "Full Stack web and mobile development",
-            "Interactive Front End as per the design",
-            "React and React Native",
-            "Redux for State Mnanagement",
-            "Building REST API",         
-            "Managing database",
-       ],
-       heading: "Here are a Few Highlights:"
+      bullets: [
+        "Full Stack web and mobile development",
+        "Interactive Front End as per the design",
+        "React and React Native",
+        "Redux for State Mnanagement",
+        "Building REST API",
+        "Managing database",
+      ],
+      heading: "Here are a Few Highlights:"
     }
-}
+  };
 
   const renderHighlights = () => {
     return (
-        screenConstants.highlights.bullets.map((value,i) => (
-            <div className="highlight" key={i}>
-                <div className="highlight-blob"></div>
-                <span>{ value }</span>
-            </div>
-        ))
-    )
-}
+      screenConstants.highlights.bullets.map((value, i) => (
+        <div className="highlight" key={i}>
+          <div className="highlight-blob"></div>
+          <span>{value}</span>
+        </div>
+      ))
+    );
+  };
 
   useEffect(() => {
     return () => {
@@ -48,7 +48,7 @@ const screenConstants = {
 
   return (
     <>
-      <div className="about-me-container screen-container fade-in" id={props.id || ''}>
+      <div className="about-me-container fade-in" id={props.id || ''}>
         <div className="about-me-parent">
           <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'} />
           <div className="about-me-card">
