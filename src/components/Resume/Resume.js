c7286ec3f11be66d412db40f5b6186fae2dee08d
gabmor38 <Gabriela.morenor@gmail.com>
2022-04-25 22:30:04 -0400

    map through skills and added html to skills

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -11,55 +11,119 @@ export default function Resume(props) {
 
   const resumeHeading = (props) => {
     <div className="resume-heading">
-        <div className="resume-main-heading">
-          <div className="heading-bullet"></div>
-          <span>{props.heading ? props.heading : ""}</span>
-          {props.fromDate && props.toDate ? (
-            <div className="heading-date">
-              {props.fromDate + "-" + props.toDate}
-            </div>
-          ) : (
-            <div></div>
-          )}
-        </div>
-        <div className="resume-sub-heading">
-          <span>{props.subHeading ? props.subHeading : ""}</span>
-        </div>
-        <div className="resume-heading-description">
-          <span>{props.description ? props.description : ""}</span>
-        </div>
+      <div className="resume-main-heading">
+        <div className="heading-bullet"></div>
+        <span>{props.heading ? props.heading : ""}</span>
+        {props.fromDate && props.toDate ? (
+          <div className="heading-date">
+            {props.fromDate + "-" + props.toDate}
+          </div>
+        ) : (
+          <div></div>
+        )}
       </div>
+      <div className="resume-sub-heading">
+        <span>{props.subHeading ? props.subHeading : ""}</span>
+      </div>
+      <div className="resume-heading-description">
+        <span>{props.description ? props.description : ""}</span>
+      </div>
+    </div>;
   };
 
   const resumeBullets = [
     { label: "Education", logoSrc: "education.svg" },
-    { label: "Work Experience", logoSrc:"work-history.svg" },
+    { label: "Work Experience", logoSrc: "work-history.svg" },
     { label: "Skills", logoSrc: "programming-skills.svg" },
-    { label: "Projects", logoSrc:"projects.svg" },
+    { label: "Projects", logoSrc: "projects.svg" },
 
   ];
 
   const skills = [
-    { skill: "Javascript",ratingPercentage: 85},
-    { skill: "React", ratingPercentage: 85},
+    { skill: "Javascript", ratingPercentage: 85 },
+    { skill: "React", ratingPercentage: 85 },
   ];
 
   const projects = [
-    { title: "React Fitness",
-    duration: {fromDate:"2022", toDate: "2022" },
-    description: "A fitness website that allows a user to create workouts for various body parts",
-    subheading: "Tech stack - PERN"
+    {
+      title: "React Fitness",
+      duration: { fromDate: "2022", toDate: "2022" },
+      description: "A fitness website that allows a user to create workouts for various body parts",
+      subheading: "Tech stack - PERN"
     },
-    { title: "Tweeter",
-    duration: {fromDate:"2022", toDate: "2022" },
-    description: "A fitness website that allows a user to create workouts for various body parts",
-    subheading: "Tech stack - PERN"
+    {
+      title: "Tweeter",
+      duration: { fromDate: "2022", toDate: "2022" },
+      description: "A fitness website that allows a user to create workouts for various body parts",
+      subheading: "Tech stack - PERN"
     },
-    { title: "Tiny App",
-    duration: {fromDate:"2022", toDate: "2022" },
-    description: "A fitness website that allows a user to create workouts for various body parts",
-    subheading: "Tech stack - PERN"
+    {
+      title: "Tiny App",
+      duration: { fromDate: "2022", toDate: "2022" },
+      description: "A fitness website that allows a user to create workouts for various body parts",
+      subheading: "Tech stack - PERN"
     },
+  ];
+
+  const resumeDetails = [
+    <div className='resume-screen-container' key="education">
+      <resumeHeading
+        heading={"Lighthouse labs"}
+        subheading={"Web Programming"}
+        fromDate={2021}
+        toDate={2022}
+      />
+      <resumeHeading
+        heading={"Algonquin College"}
+        subheading={"Accounting"}
+        fromDate={2021}
+        toDate={2022}
+      />
+
+      <resumeHeading
+        heading={"High School"}
+        subheading={"Name of highschool"}
+        fromDate={2021}
+        toDate={2022}
+      />
+
+    </div>,
+
+    <div className='resume-screen-container' key="work experience">
+      <resumeHeading
+        heading={"RCMP"}
+        subheading={"OM-02 Business Analyst"}
+        fromDate={2021}
+        toDate={"current"}
+      />
+      <div className="experience-description">
+        <span className="resume-description-text"> Currently working as a form designer</span>
+      </div>
+
+      <div className="experience-description">
+        <span className="resume-description-text"> Create and Updated Forms</span>
+        <br />
+      </div>,
+
+      <div className="resume-screen-container programming-skills-container" key="programming-skills">
+        {skills.map((skill, index) => (
+          <div className="skill-parent" key={index}>
+            <div className="heading-bullet"> 
+              <span>{skill.skill}</span>
+              <div className="skill-percentage">
+                <div style={{width: skill.ratingPercetage + "%"}} className="active-percentage">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>,
+
+      
+
+
+    </div>
   ];
 
   let fadeInScreenHandler = (screen) => {
